8578b33da186ba86907bac10e4a984d17f76b1da
Mauritz Marais <mauritz303@gmail.com>
2023-04-26 16:51:06 +0200

    updated state, handlechange function updates state correctly, added functions to handle adding and deleting form sections

--- src/App.js
+++ src/App.js
@@ -2,59 +2,111 @@ import React, { Component } from "react";
 import Form from "./components/form";
 import DisplayCV from "./components/cv";
 import './App.css';
+import { v4 as uuidv4 } from 'uuid';
 
 class App extends Component {
   constructor(props) {
     super(props)
     // applicant details
     this.state = {
-      personalDetails: {
+      personalDetails: [{
+        id: 1,
         name: '',
         email: '',
         phone: ''
-      },
-      educationDetails: {
+      }],
+      education: [{
+        id: uuidv4(),
         schoolName: '',
         areaOfStudy: '',
         graduationYear: ''
-      },
-      experienceDetails: {
+      }],
+      experience: [{
+        id: uuidv4(),
+        companyName: '',
+        positionTitle: '',
+        mainTasks: '',
+        from: '',
+        until: ''
+      }]
+    }
+
+    // bind this when passing a method
+    this.handleChange = this.handleChange.bind(this);
+
+    this.handleAddBtn = this.handleAddBtn.bind(this)
+
+    this.handleDeleteBtn = this.handleDeleteBtn.bind(this);
+  }
+
+  // update state on change
+  handleChange = (event, section, field, id) => {
+    this.setState({
+      ...this.state,
+      [section]:
+        [...this.state[section]].map(item => {
+          if (item.id === id) {
+            // Create a *new* object with changes
+            return { ...item, [field]: event.target.value };
+          } else {
+            // No changes
+            return item;
+          }
+        })
+    })
+  };
+
+  // add education / experience section to state
+  handleAddBtn = (section) => {
+    this.setState({
+      ...this.state,
+      // add new section to array
+      [section]: [...this.state[section], this.addSection(section)]
+    })
+  }
+
+  // delete education or experience section
+  handleDeleteBtn = (section, id) => {
+    this.setState({
+      ...this.state,
+      [section]: [...this.state[section]].filter(item => {
+        return item.id !== id;
+      })
+    })
+  }
+
+  // create new education or experience section
+  addSection = (sectionName) => {
+    if (sectionName === 'education') {
+      return {
+        id: uuidv4(),
+        schoolName: '',
+        areaOfStudy: '',
+        graduationYear: ''
+      }
+    }
+
+    else {
+      return {
+        id: uuidv4(),
         companyName: '',
         positionTitle: '',
         mainTasks: '',
         from: '',
         until: ''
       }
+
     }
-
-    // bind this when passing a method
-    this.onClickBtn = this.onClickBtn.bind(this);
-
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange = (event, section, field) => {
-    this.setState({
-      ...this.state,
-      [section]: {
-        ...this.state[section],
-        [field]: event.target.value
-      }
-    })
-  };
-
-  onClickBtn() {
-    console.log("clicked btn!")
   }
 
   render() {
-    const { personalDetails, educationDetails, experienceDetails } = this.state
+    const { personalDetails, education, experience } = this.state
     return (
       <>
         <header><h1>CV application</h1></header>
         <main>
-          <Form personalDetails={personalDetails} educationDetails={educationDetails} experienceDetails={experienceDetails} handleChange={this.handleChange} />
-          <DisplayCV title="CV display" personalDetails={personalDetails} educationDetails={educationDetails} experienceDetails={experienceDetails} />
+          <Form personalDetails={personalDetails} education={education} experience={experience} handleChange={this.handleChange} handleAddBtn={this.handleAddBtn} handleDeleteBtn={this.handleDeleteBtn} />
+          <DisplayCV title="CV display" personalDetails={personalDetails} education={education} experience={experience} />
         </main>
       </>
     )
